f83b7fa59e3e386eb215cc502848108de84982f5
adamdev <adamdevofficial@protonmail.com>
2024-08-04 21:00:55 -0400

    Update version number for release

--- src/Main.cs
+++ src/Main.cs
@@ -17,7 +17,7 @@ namespace NEP.MagPerception
         public const string Name = "MagPerception"; // Name of the Mod.  (MUST BE SET)
         public const string Author = null; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.2.2"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
